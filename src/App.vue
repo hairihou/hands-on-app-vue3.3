3c370a9f360ecf0053a73fb7493f6ad254473c2f
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-01-06 04:10:00 +0900

    feat(): add custom config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex flex-col gap-y-2 p-4">
+    <div
+      class="flex flex-col items-center justify-center bg-primary w-80 h-8 rounded-md"
+    >
+      <span class="text-primary-contrast">Primary Box</span>
+    </div>
+
+    <div
+      class="flex flex-col items-center justify-center bg-primary-rgb/50 w-80 h-8 rounded-md"
+    >
+      <span class="text-primary-contrast-rgb/50">Primary Box RGB</span>
+    </div>
+
+    <p class="text-primary-shade text-sm">Primary Shade</p>
+    <p class="text-primary-tint text-lg">Primary Tint</p>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#0054e9",
+      "primary-rgb": "rgb(0,84,233)",
+      "primary-contrast": "#ffffff",
+      "primary-contrast-rgb": "rgb(255,255,255)",
+      "primary-shade": "#004acd",
+      "primary-tint": "#1a65eb",
+    },
     extend: {},
   },
   plugins: [],
