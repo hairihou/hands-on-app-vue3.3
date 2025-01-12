7c4ed460946b13630d44c93420844beb135098ec
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-01-12 14:10:34 +0900

    feat(): add ci

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import vue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
+
+export default tseslint.config(
+  { ignores: ["**/dist", "**/public"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...vue.configs["flat/recommended"],
+      prettier,
+    ],
+    files: ["**/*.{ts,vue}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: vueParser,
+      parserOptions: { parser: tseslint.parser },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { fixStyle: "inline-type-imports" },
+      ],
+    },
+  },
+);
